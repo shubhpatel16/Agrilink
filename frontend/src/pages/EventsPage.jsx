c128d244173e4aa334542ade7f3c22964bf97c8a
Vaibhav Patel <vaibhavkpatel592@gmail.com>
2025-03-07 21:47:14 +0530

    Multiple events display on event page

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -6,16 +6,25 @@ import Loader from "../components/Layout/Loader";
 
 const EventsPage = () => {
   const { allEvents, isLoading } = useSelector((state) => state.events);
+
   return (
     <>
-      {/* {isLoading ? (
+      {isLoading ? (
         <Loader />
-      ) : ( */}
-      <div>
-        <Header activeHeading={4} />
-        <EventCard active={true} data={allEvents && allEvents[0]} />
-      </div>
-      {/* // )} */}
+      ) : (
+        <div>
+          <Header activeHeading={4} />
+          <div className="w-full p-4">
+            {allEvents.length > 0 ? (
+              allEvents.map((event, index) => (
+                <EventCard key={index} active={true} data={event} />
+              ))
+            ) : (
+              <h4 className="text-center w-full">No Events available!</h4>
+            )}
+          </div>
+        </div>
+      )}
     </>
   );
 };
